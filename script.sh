8ceeb0a41d2ec5d9a06c30b4297d51015525a470
Software Factory <factory@software-factory.dev>
2026-06-29 23:56:49 +0200

    feat(PLAN-01):
    Generated by gsd-executor

--- script.sh
+++ script.sh
@@ -1,5 +1,1 @@
-python3 -m py_compile src/bpa/__init__.py && echo "OK: src/bpa/__init__.py"
-python3 -m py_compile src/bpa/config.py && echo "OK: src/bpa/config.py"
-python3 -m py_compile src/bpa/db.py && echo "OK: src/bpa/db.py"
-python3 -m py_compile src/bpa/logging_setup.py && echo "OK: src/bpa/logging_setup.py"
-python3 -m py_compile src/bpa/main.py && echo "OK: src/bpa/main.py"
+pwd && ls -la
